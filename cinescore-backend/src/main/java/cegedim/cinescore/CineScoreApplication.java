6e64e44d55532d68147dde0b331deec212d3e9d9
Iancu Radu <raducorneliu.iancu@cegedim.com>
2026-09-19 16:16:24 +0300

    Created empty project

---
+++ cinescore-backend/src/main/java/cegedim/cinescore/CineScoreApplication.java
@@ -0,0 +1,13 @@
+package cegedim.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineScoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(cegedim.movies.CineScoreApplication.class, args);
+	}
+
+}
---
+++ cinescore-backend/src/test/java/cegedim/cinescore/CineScoreApplicationTests.java
@@ -0,0 +1,13 @@
+package cegedim.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineScoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
