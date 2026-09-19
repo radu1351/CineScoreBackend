f87b5285ccde7aaeae4abe7f07b4e0eefae10895
Iancu Radu <raducorneliu.iancu@cegedim.com>
2026-09-19 16:30:16 +0300

    Modified project structure

---
+++ src/main/java/cegedim/cinescore/CineScoreApplication.java
@@ -0,0 +1,12 @@
+package cegedim.cinescore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineScoreApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CineScoreApplication.class, args);
+	}
+
+}
